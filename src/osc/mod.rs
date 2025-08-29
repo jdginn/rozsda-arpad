5571b087ef78e692cca872b40743c14c6f66bb97
Justin Ginn <Justindavid.ginn@gmail.com>
2025-08-30 00:43:16 +0200

    Move context_gate to its own directory

--- src/osc/mod.rs
+++ src/osc/mod.rs
@@ -1,2 +1,2 @@
-pub mod context_gate;
 pub mod generated_osc;
+pub mod route_context;
---
+++ src/osc/route_context/mod.rs
@@ -0,0 +1,5 @@
+pub mod context_gate;
+
+pub use context_gate::{ContextGateBuilder, ContextKindTrait, ContextTrait, OscGatedRouterBuilder};
+
+mod tests;
