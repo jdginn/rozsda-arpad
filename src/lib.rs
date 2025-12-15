6a8a20e8a3ee6752a9e7e5968dda3a013ae2f8ac
Justin Ginn <Justindavid.ginn@gmail.com>
2025-12-16 00:35:21 +0100

    Add TrackData
    Statefully store track information and allow updating it, passing update
    messages upstream or downstream, and querying track info on demand.
    
    Adds various Msg types for sending track-related information.

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod traits;
 
 pub mod midi;
 pub mod osc;
+pub mod track;
---
+++ src/track/track.rs
@@ -0,0 +1,207 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+use std::thread;
+
+use crossbeam_channel::{Receiver, Sender, bounded};
+
+use bimap::BiMap;
+
+#[derive(Clone)]
+pub enum Direction {
+    Upstream,
+    Downstream,
+}
+
+#[derive(Clone)]
+pub enum TrackMsg {
+    TrackDataMsg(TrackDataMsg),
+    TrackQuery(TrackQuery),
+}
+
+#[derive(Clone)]
+pub struct TrackDataMsg {
+    pub guid: String,
+    pub direction: Direction,
+    pub data: DataPayload,
+}
+
+#[derive(Clone)]
+pub struct TrackQuery {
+    pub guid: String,
+    pub direction: Direction,
+}
+
+#[derive(Clone)]
+pub enum DataPayload {
+    Volume(f32),
+    Pan(f32),
+    Name(String),
+    Selected(bool),
+    Muted(bool),
+    Soloed(bool),
+    Armed(bool),
+    TrackData(TrackData),
+}
+
+#[derive(Clone)]
+struct TrackData {
+    guid: String,
+    name: String,
+    selected: bool,
+    muted: bool,
+    soloed: bool,
+    armed: bool,
+    volume: f32,
+    pan: f32,
+}
+
+// To do this the normal way with putting Bind/Set on each field involves a lot of boilerplate...
+// we have to make something like TrackDataNameAccessor that implements the traits and then expose
+// that via TrackData.name().bind() etc.
+
+impl TrackData {
+    fn new(guid: &str) -> Self {
+        Self {
+            guid: guid.to_string(),
+            name: String::new(),
+            selected: false,
+            muted: false,
+            soloed: false,
+            armed: false,
+            volume: 0.0,
+            pan: 0.0,
+        }
+    }
+}
+
+// This basically should expose the Binds on an underlying TrackData but keep TrackManager locked
+// while we do so
+struct TrackAccessor<'a> {
+    guard: std::sync::MutexGuard<'a, ()>,
+    manager: &'a mut TrackManager,
+    guid: String,
+    downstream: Sender<TrackMsg>,
+}
+
+impl TrackAccessor<'_> {
+    fn set_name(&mut self, name: String) {
+        if let Some(track) = self.manager.tracks.get_mut(&self.guid) {
+            track.name = name;
+        }
+    }
+}
+
+pub struct TrackManager {
+    mux: Mutex<()>,
+    tracks: HashMap<String, TrackData>,
+    input: Receiver<TrackMsg>,
+    downstream: Sender<TrackMsg>,
+    upstream: Sender<TrackMsg>,
+}
+
+impl TrackManager {
+    pub fn start(
+        input: Receiver<TrackMsg>,
+        upstream: Sender<TrackMsg>,
+        downstream: Sender<TrackMsg>,
+    ) {
+        thread::spawn(move || {
+            let mut manager = Self {
+                mux: Mutex::new(()),
+                tracks: HashMap::new(),
+                input,
+                downstream,
+                upstream,
+            };
+            loop {
+                manager.handle_messages();
+            }
+        });
+    }
+
+    pub fn handle_messages(&mut self) {
+        while let Ok(msg) = self.input.try_recv() {
+            match msg {
+                TrackMsg::TrackDataMsg(msg) => {
+                    let msg_cloned = msg.clone();
+                    // If we've never seen this track before, create a new entry
+                    let track = self
+                        .tracks
+                        .entry(msg.guid.to_string())
+                        .or_insert_with(|| TrackData::new(&msg.guid));
+                    // Update the track data based on the message
+                    match msg.data {
+                        DataPayload::Volume(volume) => {
+                            track.volume = volume;
+                            println!("Track {} volume set to {}", msg.guid, volume);
+                        }
+                        DataPayload::Pan(pan) => {
+                            track.pan = pan;
+                            println!("Track {} pan set to {}", msg.guid, pan);
+                        }
+                        DataPayload::Name(name) => {
+                            track.name = name.clone();
+                            println!("Track {} name set to {}", msg.guid, name);
+                        }
+                        DataPayload::Selected(selected) => {
+                            track.selected = selected;
+                            println!("Track {} selected set to {}", msg.guid, selected);
+                        }
+                        DataPayload::Muted(muted) => {
+                            track.muted = muted;
+                            println!("Track {} muted set to {}", msg.guid, muted);
+                        }
+                        DataPayload::Soloed(soloed) => {
+                            track.soloed = soloed;
+                            println!("Track {} soloed set to {}", msg.guid, soloed);
+                        }
+                        DataPayload::Armed(armed) => {
+                            track.armed = armed;
+                            println!("Track {} armed set to {}", msg.guid, armed);
+                        }
+                        // Update everything!
+                        DataPayload::TrackData(track_data) => {
+                            *track = track_data;
+                        }
+                    }
+                    // Forward the message to the appropriate place
+                    match msg.direction {
+                        Direction::Upstream => {
+                            self.upstream
+                                .send(TrackMsg::TrackDataMsg(msg_cloned))
+                                .unwrap();
+                        }
+                        Direction::Downstream => {
+                            self.downstream
+                                .send(TrackMsg::TrackDataMsg(msg_cloned))
+                                .unwrap();
+                        }
+                    }
+                }
+                TrackMsg::TrackQuery(msg) => match msg.direction {
+                    // Respond with ALL of the current track data
+                    Direction::Upstream => {
+                        if let Some(track) = self.tracks.get(&msg.guid) {
+                            let response = TrackMsg::TrackDataMsg(TrackDataMsg {
+                                guid: msg.guid.clone(),
+                                direction: Direction::Upstream, // Don't care?
+                                data: DataPayload::TrackData(track.clone()),
+                            });
+                            self.upstream.send(response).unwrap();
+                        }
+                    }
+                    Direction::Downstream => {
+                        if let Some(track) = self.tracks.get(&msg.guid) {
+                            let response = TrackMsg::TrackDataMsg(TrackDataMsg {
+                                guid: msg.guid.clone(),
+                                direction: Direction::Downstream, // Don't care?
+                                data: DataPayload::TrackData(track.clone()),
+                            });
+                            self.downstream.send(response).unwrap();
+                        }
+                    }
+                },
+            }
+        }
+    }
+}
