58f65b9360e76f1c21e181f75491da72aedf7caa
Justin Ginn <Justindavid.ginn@gmail.com>
2026-04-24 19:12:45 +0300

    Check in src/modes/button.rs

---
+++ src/modes/button.rs
@@ -0,0 +1,23 @@
+#[derive(Clone, Copy, PartialEq)]
+pub struct Button {
+    state: bool,
+}
+
+impl Button {
+    pub fn new() -> Self {
+        Button { state: false }
+    }
+
+    pub fn is_on(&self) -> bool {
+        self.state
+    }
+
+    pub fn set(&mut self, new_state: bool) {
+        self.state = new_state;
+    }
+
+    pub fn toggle(&mut self) -> bool {
+        self.state = !self.state;
+        self.state
+    }
+}
