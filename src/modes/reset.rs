5b3d75fe127c088d995ef02ddd29a7ae8ab41390
Justin Ginn <Justindavid.ginn@gmail.com>
2026-08-16 12:45:19 +0200

    squash reset: remove println

--- src/modes/reset.rs
+++ src/modes/reset.rs
@@ -10,7 +10,6 @@ pub fn reset_hardware(io: &mut dyn UpstreamIo, num_channels: usize) {
             }
             .into(),
         );
-        println!("Resetting select for {:?}", i);
         io.send_to_v1m(
             v1m::SelectLEDMsg {
                 idx: i as i32,
